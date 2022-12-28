d4ed14785a8b1d09d8c94bc947058e34bd6b3430
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-12-28 09:28:07 -0500

    se eliminan los controladores creados por defecto al crear el modelo y se crean nuevos controladores en la carpeta API

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\CategoryController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -14,9 +15,20 @@ Route::get('/hello_world', function (){
 });
 
 Route::controller(UserController::class)->group(function (){
-    Route::get('/users/{username}', 'show')->name('users.show');
+    Route::get('/users/{username}', 'show')->name('api.v1.users.show');
 
-    Route::post('/users/store', 'store')->name('users.store');
+    Route::post('/users/store', 'store')->name('api.v1.users.store');
 });
+
+Route::controller(CategoryController::class)->group(function (){
+    Route::get('/categories/', 'index')->name('api.v1.categories.index');
+    Route::get('/categories/{category}', 'show')->name('api.v1.categories.show');
+
+    Route::post('/categories/', 'store')->name('api.v1.categories.store');
+    Route::put('/categories/{category}', 'store')->name('api.v1.categories.update');
+    Route::delete('/category/{category}', 'delete')->name('api.v1.categories.delete');
+});
+
+//Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
 
 
