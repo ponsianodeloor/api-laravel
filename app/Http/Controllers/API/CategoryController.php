4a08a1894607e3549ab33bc33f8161c880be0ccc
Ponsiano De Loor <ponsianodeloor@gmail.com>
2023-01-03 09:18:49 -0500

    CRUD de categorias

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -9,7 +9,9 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index(){
+        $categories = Category::all();
 
+        return json_decode($categories, 200);
     }
 
     public function create(){
@@ -17,11 +19,18 @@ class CategoryController extends Controller
     }
 
     public function store(Request $request){
+        $request->validate([
+            'name'=> 'required|max:255',
+            'slug' => 'required|max:255|unique:categories'
+        ]);
 
+        $category = Category::create($request->all());
+
+        return json_decode($category, 200);
     }
 
     public function show(Category $category){
-
+        return json_decode($category, 200);
     }
 
     public function edit(Category $category){
@@ -29,10 +38,19 @@ class CategoryController extends Controller
     }
 
     public function update(Request $request, Category $category){
+        $request->validate([
+            'name'=> 'required|max:255',
+            'slug' => 'required|max:255|unique:categories'
+        ]);
 
+        $category->update($request->all());
+
+        return json_decode($category, 200);
     }
 
     public function destroy(Category $category){
+        $category->delete();
 
+        return $category;
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,11 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
     //relacion uno a muchos
     public function posts(){
         return $this->hasMany(Post::class);
